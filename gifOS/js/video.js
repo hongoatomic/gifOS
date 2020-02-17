50c09ea2fe340601c8084a94d9f463726ef92e68
Hongoatomic <m.sol.escobar@gmail.com>
2020-02-17 15:31:37 -0300

    work on video record and stop

--- gifOS/js/video.js
+++ gifOS/js/video.js
@@ -1,32 +1,106 @@
 async function getStreamAndRecord() {
+    let stream = null;
+    let recorder = null;
+    let gifStream = null;
+    let gifTime = null;
+    let blobFile = null;
+    const userVideo = document.querySelector("video");
+    async function showUserVideo() {
+        const video = userVideo;
+        stream = await navigator.mediaDevices.getUserMedia({
+            audio: false,
+            video: {
+                height: { max: 480 },
+                width: { ideal: 860 }
+            }
+        });
+        video.srcObject = stream;
+        video.play();
+    }
+
+    async function startRecord() {
+        userVideo.load();
+        userVideo.play();
+        recorder = new RecordRTCPromisesHandler(stream, {
+            type: "video"
+        });
+        gifStream = new RecordRTCPromisesHandler(stream, {
+            type: "gif"
+        });
+        await recorder.startRecording();
+        await gifStream.startRecording();
+        recorder.stream = stream;
+        gifStream.stream = stream;
+    }
+
+    async function stopRecord() {
+        userVideo.srcObject = null;
+        await recorder.stopRecording();
+        await gifStream.stopRecording();
+        const videoBlob = await recorder.getBlob();
+        const gifBlob = await gifStream.getBlob();
+        blobFile = gifBlob;
+        userVideo.src = URL.createObjectURL(videoBlob);
+        stream.getTracks().forEach(track => {
+            track.stop();
+        });
+        await recorder.reset();
+        await gifStream.reset();
+        await recorder.destroy();
+        await gifStream.destroy();
+        recorder = null;
+        gifStream = null;
+    }
+    function createFormData() {
+        const form = new FormData();
+        form.append("file", blobFile, "my-guifos.gif");
+        blobFile = null;
+        return form;
+    }
+
+    function saveGif(url) {
+        const newId = localStorage.length;
+        localStorage.setItem(`my-guifos-${newId}`, url);
+    }
+    const grabar = document.getElementById("record");
+    grabar.addEventListener("click", () => {
+        startRecord();
+        showUserVideo();
+    });
+
+    const parar = document.getElementById("stop");
+    parar.addEventListener("click", () => {
+        stopRecord();
+        createFormData();
+        saveGif(url);
+    });
+}
+getStreamAndRecord();
+
+/*let recorder = RecordRTC(stream, {
+        type: "video"
+    });
     navigator.mediaDevices
         .getUserMedia({
             video: true,
-            audio: true
+            audio: false
         })
         .then(async function(stream) {
-            let recorder = RecordRTC(stream, {
-                type: "video"
-            });
             recorder.startRecording();
 
             /*const sleep = m => new Promise(r => setTimeout(r, m));
             await sleep(3000);*/
-
-            function stop() {
-                recorder.stopRecording(function() {
-                    let blob = recorder.getBlob();
-                    invokeSaveAsDialog(blob);
-                });
-            }
+/* });
+    async function stop() {
+        recorder.stopRecording(function() {
+            let blob = recorder.getBlob();
+            invokeSaveAsDialog(blob);
         });
+    }
+    
+    });
 }
-const grabar = document.getElementById("record");
-grabar.addEventListener("click", () => {
-    getStreamAndRecord();
-});
 
-const stop = document.getElementById("stop");
-stop.addEventListener("click", () => {
-    stop();
-});
+
+
+});*/
