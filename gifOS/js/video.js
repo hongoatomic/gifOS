d538cb38b011b2a230dcf81c122820573d725ea9
Hongoatomic <m.sol.escobar@gmail.com>
2020-02-23 17:22:06 -0300

    add video

--- gifOS/js/video.js
+++ gifOS/js/video.js
@@ -1,57 +1,120 @@
-function getStreamAndRecord() {
-    navigator.mediaDevices
+let comenzar = document.getElementById("record");
+let detener = document.getElementById("stop");
+let video = document.querySelector("video");
+let recorder;
+const apiKey = "api_key=QdQJ4v523JXYS55K6OWuPzbw5U0Cqw1p";
+
+comenzar.onclick = function() {
+    record();
+};
+detener.onclick = function() {
+    stop();
+};
+
+/* RECORD */
+function record() {
+    this.disabled = true;
+    getStreamAndRecord(function(stream) {
+        /* set video tag data */
+        video.muted = true;
+        video.volume = 0;
+        video.srcObject = stream;
+
+        recorder = RecordRTC(stream, {
+            type: "gif",
+            frameRate: 1,
+            quality: 10,
+            width: 360,
+            hidden: 240,
+            onGifRecordingStarted: function() {
+                console.log("started");
+            }
+        });
+
+        recorder.startRecording();
+
+        // release stream on stopRecording
+        recorder.stream = stream;
+        detener.disabled = false;
+    });
+}
+
+function getStreamAndRecord(callback) {
+    navigator.mediaDevices //para permitir que el navegador acceda a los devices
         .getUserMedia({
             audio: false,
-            video: {
-                height: { max: 480 }
-            }
+            video: true
         })
         .then(function(stream) {
-            video.srcObject = stream;
-            video.play();
+            callback(stream);
+        })
+        .catch(function(error) {
+            alert("Unable to capture your camera. Please check console logs.");
+            console.error(error);
         });
 }
 
-let stream = navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-});
-let recorder = new RecordRTCPromisesHandler(stream, {
-    type: "video"
-});
+/* STOP RECORDING */
+function stop() {
+    this.disabled = true;
+    recorder.stopRecording(stopRecordingCallback);
+}
 
-const createGif = {
-    userVideo: document.querySelector("video"),
-    stream: null,
-    recorder: null,
-    gifStream: null,
-    gifTime: null,
-    blobFile: null,
+function stopRecordingCallback() {
+    video.src = video.srcObject = null;
+    video.muted = false;
+    video.volume = 1;
+    video.src = URL.createObjectURL(recorder.getBlob());
 
-    startRecord: async function() {
-        createGif.userVideo.stream.play();
-        recorder.startRecording();
-    },
-    stopRecord: async function(blobURL) {
-        await recorder.stopRecording();
-        let blob = await recorder.getBlob();
-        invokeSaveAsDialog(blob);
-        await createGif.recorder.reset();
-        await createGif.gifStream.reset();
-        await createGif.recorder.destroy();
-        await createGif.gifStream.destroy();
-        createGif.recorder = null;
-        createGif.gifStream = null;
-    },
-    save: function(fileName) {}
-};
+    recorder.stream.stop();
 
-let comenzar = document.getElementById("record");
-let detener = document.getElementById("stop");
+    /* Create the FormData to send to giphy */
+    let form = new FormData();
+    var blob = recorder.getBlob();
+    form.append("file", blob, "myGif.gif");
+    //console.log(form.get('file'));
 
-comenzar.onclick = function() {
-    createGif.startRecord();
-};
-detener.onclick = function() {
-    createGif.stopRecord();
-};
+    postToGiphy(form).then(response => {
+        /* This is the specific id of the gif updated to giphy (can verified the id here https://giphy.com/channel/solescobar10c6) */
+        console.log("Response de giphy " + response.data);
+        const id = response.data.id;
+
+        const endpoint = "https://api.giphy.com/v1/gifs/";
+        /* To find the url of this new gif hacemos como el fetch en 'index.js' */
+        fetch(endpoint + id + "?" + apiKey)
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(json) {
+                console.log(json.data);
+                const url = json.data.images.fixed_height.url;
+
+                /* registrar el nuevo gif en local storage con sur url */
+                /* localStorage.length will help for the loop to get them back after, first one saved will be myGifos-1, second one myGifos-2... */
+                localStorage.setItem("myGifos-" + localStorage.length, url);
+            });
+    });
+
+    recorder.destroy();
+    recorder = null;
+}
+
+/* Doc here https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch */
+
+async function postToGiphy(formData) {
+    const pathToUpload = "https://upload.giphy.com/v1/gifs?";
+    const parameterToUpload = {
+        headers: new Headers(), // Create empty header
+        body: formData, // The gif we send (in function parameter)
+        method: "POST",
+        mode: "cors"
+    };
+
+    /** Upload with fetch :
+     *   - First parameter will be the endpoint URL : https://upload.giphy.com/v1/gifs?api_key=QdQJ4v523JXYS55K6OWuPzbw5U0Cqw1p
+     *   - Second parameter will be the gif (formData)
+     */
+    const response = await fetch(pathToUpload + apiKey, parameterToUpload);
+
+    return await response.json();
+}
