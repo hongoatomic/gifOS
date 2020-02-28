59f717f07233704e3fc6283c27eda9fd8cd5322b
Hongoatomic <m.sol.escobar@gmail.com>
2020-02-27 22:10:02 -0300

    add tags and suggestions

--- gifOS/js/index.js
+++ gifOS/js/index.js
@@ -26,6 +26,24 @@ const trendingEl = document.getElementById("trending");
 const randomGif = document.getElementById("random-gif");
 const fakeInput = document.querySelector("h3.fakeInput");
 const fakeInput2 = document.querySelector("h3.fakeInput:last-of-type");
+let tags = [];
+let resultadoSugerido = document.getElementById("resultadoSugerido");
+let resultadosSugeridos1 = ["cat", "dog", "unicorn", "mouse"];
+let resultadosSugeridos2 = ["orange", "banana", "apple", "grape"];
+let resultadosSugeridos3 = ["html", "css", "javascript", "java"];
+let aleatorio = Math.round(Math.random() * 4);
+let resultSugeridos1 = document.createElement("input");
+let resultSugeridos2 = document.createElement("input");
+let resultSugeridos3 = document.createElement("input");
+resultSugeridos1.value = resultadosSugeridos1[aleatorio];
+resultSugeridos2.value = resultadosSugeridos2[aleatorio];
+resultSugeridos3.value = resultadosSugeridos3[aleatorio];
+resultSugeridos1.id = "search-sugerencia1";
+resultSugeridos2.id = "search-sugerencia2";
+resultSugeridos3.id = "search-sugerencia3";
+resultSugeridos1.style.cursor = "pointer";
+resultSugeridos2.style.cursor = "pointer";
+resultSugeridos3.style.cursor = "pointer";
 
 searchForm.addEventListener("submit", function(e) {
     e.preventDefault();
@@ -37,34 +55,76 @@ searchForm.addEventListener("submit", function(e) {
     fakeInput2.innerHTML = "";
     fakeInput.style.border = "none";
     fakeInput2.style.border = "none";
+    crearTags();
 
-    let resultadoSugerido = document.getElementById("resultadoSugerido");
-    let resultadosSugeridos = ["cat", "dog", "unicorn", "mouse"];
-    let aleatorio = Math.round(Math.random() * 4);
-
-    resultadoSugerido.innerHTML =
-        ` <input
-            id="search-input"
-            type="text"
-            value="${resultadosSugeridos[aleatorio]}"
-            autocomplete="off"
-            disabled
-        />` +
-        ` <input
-        id="search-input"
-        type="text"
-        value="Resultado sugerido"
-        autocomplete="off"
-        disabled
-    />` +
-        ` <input
-    id="search-input"
-    type="text"
-    value="Resultado sugerido"
-    autocomplete="off"
-    disabled
-/>`;
+    resultadoSugerido.appendChild(resultSugeridos1);
+    resultadoSugerido.appendChild(resultSugeridos2);
+    resultadoSugerido.appendChild(resultSugeridos3);
+    //     resultadoSugerido.innerHTML =
+    //         `        <input
+    //             id="search-sugerencia1"
+    //             style="cursor:pointer"
+    //             type="text"
+    //             value="${resultadosSugeridos1[aleatorio]}"
+    //             autocomplete="off"
+    //             disabled
+    //         />` +
+    //         ` <input
+    //         id="search-sugerencia2"
+    //         style="cursor:pointer"
+    //         type="text"
+    //         value="${resultadosSugeridos2[aleatorio]}"
+    //         autocomplete="off"
+    //         disabled
+    //     />` +
+    //         ` <input
+    //     id="search-sugerencia3"
+    //     style="cursor:pointer"
+    //     type="text"
+    //     value="${resultadosSugeridos3[aleatorio]}"
+    //     autocomplete="off"
+    //     disabled
+    // />`;
 });
+aleatorio = Math.round(Math.random() * 4);
+resultSugeridos1.onclick = function() {
+    search(resultSugeridos1.value);
+    searchInput.value = resultSugeridos1.value;
+    resultSugeridos1.value = resultadosSugeridos1[aleatorio];
+    resultSugeridos2.value = resultadosSugeridos2[aleatorio];
+    resultSugeridos3.value = resultadosSugeridos3[aleatorio];
+};
+
+resultSugeridos2.onclick = function() {
+    search(resultSugeridos2.value);
+    searchInput.value = resultSugeridos2.value;
+    resultSugeridos1.value = resultadosSugeridos1[aleatorio];
+    resultSugeridos2.value = resultadosSugeridos2[aleatorio];
+    resultSugeridos3.value = resultadosSugeridos3[aleatorio];
+};
+
+resultSugeridos3.onclick = function() {
+    search(resultSugeridos3.value);
+    searchInput.value = resultSugeridos3.value;
+    resultSugeridos1.value = resultadosSugeridos1[aleatorio];
+    resultSugeridos2.value = resultadosSugeridos2[aleatorio];
+    resultSugeridos3.value = resultadosSugeridos3[aleatorio];
+};
+let i = 0;
+
+function crearTags() {
+    let padre = resultsEl.parentNode;
+
+    tags.push("#" + searchInput.value + " ");
+
+    let tag = document.createElement("button");
+    console.log(padre);
+    padre.insertBefore(tag, resultsEl);
+    tag.innerHTML = tags[i];
+    tag.style.background = "blue";
+    i++;
+}
+
 // funcion search
 function search(q) {
     let apikey = "QdQJ4v523JXYS55K6OWuPzbw5U0Cqw1p";
